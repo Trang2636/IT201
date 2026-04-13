1f00a9ecdb0f7fa7d33ee009f3728b5d802d1dd9
Trần Thị Thu Trang <trangmaria123499@gmail.com>
2026-04-13 23:01:12 +0700

    Add files via upload

---
+++ Seasion7_Bai1/src/main/java/org/example/seasion7_bai1/bai1.md
@@ -0,0 +1,3 @@
+tên cửa hàng bị null vì nó đang không khớp với tên thuộc tính được khai báo bên model
+model đang là name nhưng ở kia đang gọi vào là restaurantName
+và bên model đang để là boolean nhưng value truyền vào lại là mở cửa 
---
+++ Seasion7_Bai1/src/main/java/org/example/seasion7_bai1/model/RestaurantProfile.java
@@ -0,0 +1,33 @@
+package org.example.seasion7_bai1.model;
+
+public class RestaurantProfile {
+    private String name;
+    private String phone;
+    private boolean active;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+
+}
